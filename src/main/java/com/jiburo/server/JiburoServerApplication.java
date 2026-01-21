e2789cf4c9ee0fdb81692ef4342f7f8051888632
roa7717 <roa7717@kcert.co.kr>
2026-01-21 14:13:42 +0900

    프로젝트 초기 init

---
+++ src/main/java/com/jiburo/server/JiburoServerApplication.java
@@ -0,0 +1,13 @@
+package com.jiburo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiburoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiburoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiburo/server/JiburoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiburo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiburoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
