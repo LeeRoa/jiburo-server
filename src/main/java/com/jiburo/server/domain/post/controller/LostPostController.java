c54dd4e4cfb2e696a32ff0f6aee188bde322518f
roa7717 <roa7717@kcert.co.kr>
2026-01-27 10:48:06 +0900

    LostPost create 불필요한 어노테이션 제거

--- src/main/java/com/jiburo/server/domain/post/controller/LostPostController.java
+++ src/main/java/com/jiburo/server/domain/post/controller/LostPostController.java
@@ -9,7 +9,6 @@ import com.jiburo.server.domain.user.dto.CustomOAuth2User;
 import com.jiburo.server.global.domain.CodeConst;
 import com.jiburo.server.global.log.annotation.AuditLog;
 import com.jiburo.server.global.response.ApiResponse;
-import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -30,7 +29,7 @@ public class LostPostController {
     @AuditLog(action = CodeConst.LogAction.POST_CREATE)
     @PostMapping
     public ApiResponse<Long> createPost(
-            @Parameter(hidden = true) @AuthenticationPrincipal CustomOAuth2User user,
+            @AuthenticationPrincipal CustomOAuth2User user,
             @Valid @RequestBody LostPostCreateRequestDto requestDto
     ) {
         Long postId = lostPostService.create(user.getUserId(), requestDto);
