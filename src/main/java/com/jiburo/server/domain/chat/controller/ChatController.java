c3bc09b3e40e3097844a4e7d4e46920a060ad0e7
roa7717 <roa7717@kcert.co.kr>
2026-03-04 15:15:06 +0900

    ChatController AuditLog 어노테이션 제거

--- src/main/java/com/jiburo/server/domain/chat/controller/ChatController.java
+++ src/main/java/com/jiburo/server/domain/chat/controller/ChatController.java
@@ -4,26 +4,15 @@ import com.jiburo.server.domain.chat.dto.ChatMessageRequestDto;
 import com.jiburo.server.domain.chat.dto.ChatMessageResponseDto;
 import com.jiburo.server.domain.chat.service.ChatMessageService;
 import com.jiburo.server.domain.user.dto.CustomOAuth2User;
-import com.jiburo.server.global.domain.CodeConst;
-import com.jiburo.server.global.log.annotation.AuditLog;
 import com.jiburo.server.global.util.HashidsUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.security.Principal;
-import java.util.List;
-import java.util.UUID;
 
 @Slf4j
 @Controller
@@ -36,7 +25,6 @@ public class ChatController {
      * 클라이언트가 메시지를 보낼 때 호출되는 핸들러
      * 프론트엔드 요청 경로: /pub/chat/rooms/{roomId}
      */
-    @AuditLog(action = CodeConst.LogAction.CHAT_MESSAGE_SEND)
     @MessageMapping("/chat/rooms/{roomId}")
     public void sendMessage(
             @DestinationVariable String roomId,
